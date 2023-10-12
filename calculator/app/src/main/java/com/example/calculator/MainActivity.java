1bbe1bf2d8e130607e41e3523dcca81ae0607ffc
arcziosppl <arturmeus230@gmail.com>
2023-10-12 22:07:49 +0200

    gradient as background

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button num0,num1,num2,num3,num4,num5,num6,num7,num8,num9,add,sub,mul,div,clear,equal;
     TextView result;
 
-    public void clear_effect(){
+    private void clear_effect(){
         result.setText("");
         effect = "";
     }
